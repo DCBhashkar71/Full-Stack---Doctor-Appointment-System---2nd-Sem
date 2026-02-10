bfb08c0dd1cebe719f833e30477fd56697abcf4b
Bhashkar <bhashkar.dc7@gmail.com>
2026-02-10 18:53:58 +0530

    New image

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SpecialityMenu = () => {
+  return (
+    <div id='speciality'>
+      <h1>
+        Find By Speciality
+      </h1>
+      <p>
+        Explore our comprehensive list of medical specialties and find the right doctor for your specific healthcare needs. Whether you're seeking a general practitioner, a specialist in cardiology, dermatology, pediatrics, or any other field, our platform allows you to easily search and connect with qualified doctors in your area. Browse through our extensive directory of medical specialties to find the expertise you require for your health and well-being.
+      </p>
+    </div>
+  )
+}
+
+export default SpecialityMenu
